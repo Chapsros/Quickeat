e0f795323d0d5021947c95fdf9bdf79579df6848
Fabien <fabien.augros@ynov.com>
2021-01-25 15:45:25 +0100

    2

--- src/Form/RestaurantType.php
+++ src/Form/RestaurantType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Restaurant;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +15,23 @@ class RestaurantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('categories')
-            ->add('imageFilename')
-            ->add('Localisation')
-            ->add('user')
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+
+            ])
+            ->add('categories', ChoiceType::class, [
+                'choices' => [
+                    'Fast Food' => 'Fast food',
+                    'Pizzeria' => 'Pizzeria',
+                    'Asiatique' => 'Asiatique',
+                    'Indien' => 'Indien',
+                    'Halal' => 'Halal',
+                    'Francais' => 'Francais',
+                    'Italien' => 'Italien',
+                    'Vegan' => 'Vegan',                    
+                ],
+            ])
+            ->add('localisation', LocalisationType::class)
         ;
     }
 
